a0508726f529f9036ae728059161c71659932dc9
hira <c0a2312799@edu.teu.ac.jp>
2024-09-24 15:46:02 +0900

    練習6:背景画像を動かす＋間延び解消

--- pygame_image.py
+++ pygame_image.py
@@ -17,7 +17,8 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        screen.blit(bg_img, [0, 0])#スクリーン（左上を）に画像を貼り付ける
+        x = -(tmr % 800)
+        screen.blit(bg_img, [x, 0])#スクリーンに画像を貼り付ける
         screen.blit(kk_img, [300, 200])#練習4
         pg.display.update()
         tmr += 1        
