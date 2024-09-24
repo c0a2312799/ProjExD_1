e32dacfe47c4777333ea1e97711590e7e72ca058
hira <c0a2312799@edu.teu.ac.jp>
2024-09-24 17:20:26 +0900

    演習１

--- pygame_image.py
+++ pygame_image.py
@@ -16,10 +16,15 @@ def main():
     kk_rct = kk_img.get_rect()#練習８-1:surfaceからrectを修得する
     kk_rct.center = 300, 200#練習8-2:Rectを使った初期座標の設定
     tmr = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        
         key_lst = pg.key.get_pressed()#練習8-3:キーの押下状態を修得する
+        if not any(key_lst):
+            kk_rct.move_ip((-1, 0))
+
         if key_lst[pg.K_UP]: #上→キーがTrueなら
             kk_rct.move_ip((0, -1))#こうかとんの縦座標を-1する
         if key_lst[pg.K_DOWN]:
@@ -27,7 +32,7 @@ def main():
         if key_lst[pg.K_LEFT]:
             kk_rct.move_ip((-1, 0))#こうかとんの横座標を-1する
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((+1, 0))#こうかとんの横座標を-1する
+            kk_rct.move_ip((+2, 0))#こうかとんの横座標を-1する
 
         x = -(tmr % 3200)
         screen.blit(bg_img, [x, 0])#スクリーンに画像を貼り付ける/練習６間延び解消背景動かす
@@ -35,10 +40,10 @@ def main():
         screen.blit(bg_img, [x + 3200, 0])
         screen.blit(bg_img2, [x + 4800, 0])
         screen.blit(kk_img, kk_rct)#練習4 ->練習8-5
+
         pg.display.update()
         tmr += 1        
         clock.tick(200)#練習5
-
 
 if __name__ == "__main__":
     pg.init()
